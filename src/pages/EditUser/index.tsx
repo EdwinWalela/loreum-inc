4bb1f6ec1ce7498bc143f741380c281aa563492d
edwinwalela <edwinwalela@gmail.com>
2023-01-13 16:07:58 +0300

    create edit user page

--- src/pages/EditUser/index.tsx
+++ src/pages/EditUser/index.tsx
@@ -3,7 +3,89 @@ import { useParams } from 'react-router-dom';
 const EditUserPage = () => {
 	const { id } = useParams();
 
-	return <div className="bg-gray-200 h-full"></div>;
+	return (
+		<div className="bg-gray-200 py-8 ">
+			<div className="flex flex-col items-center  px-6 py-8 mx-auto md:h-screen lg:py-0">
+				<div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+						<form className="space-y-4 md:space-y-6" action="#">
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Full name
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="name@company.com"
+									required={true}
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Email Address
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="name@company.com"
+									required={true}
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Occupation
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="name@company.com"
+									required={true}
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="email"
+									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>
+									Bio
+								</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									placeholder="name@company.com"
+									required={true}
+								/>
+							</div>
+
+							<button
+								type="submit"
+								className="w-full bg-black text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+							>
+								Save Changes
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default EditUserPage;
