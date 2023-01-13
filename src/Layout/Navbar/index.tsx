4bddd8b62a82b056f76b352be4f3b5167ec9edf1
edwinwalela <edwinwalela@gmail.com>
2023-01-13 16:57:35 +0300

    update header height

--- src/Layout/Navbar/index.tsx
+++ src/Layout/Navbar/index.tsx
@@ -2,7 +2,7 @@ import ItemList from './itemList';
 
 const NavBar = () => {
 	return (
-		<div className="md:w-1/5 min-w-max h-full pt-5 bg-slate-700 text-slate-200">
+		<div className="md:w-1/5 min-w-max md:h-full pt-5 bg-slate-700 text-slate-200">
 			<h1 className="font-bold text-center md:text-xl mb-3 md:mb-0">Loreum Inc</h1>
 			<ItemList />
 		</div>
--- src/pages/Landing/table.tsx
+++ src/pages/Landing/table.tsx
@@ -2,15 +2,21 @@ import NotFoundPage from '../NotFound';
 import TableHeader from './tableHeader';
 import TableRow from './tableRow';
 import { NavLink } from 'react-router-dom';
-import notfoundIcon from '../../assets/not-found.svg';
 
 const Table = () => {
 	return (
 		<>
 			<table className="md:w-11/12 md:mx-auto mx-5  my-8 mb-16  text-left shadow-md ">
 				<TableHeader />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
 			</table>
-			<NotFoundPage isPage={false} />
+			{/* <NotFoundPage isPage={false} /> */}
 		</>
 	);
 };
