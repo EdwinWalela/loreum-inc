39f3e4ed9538c2534daad131249b3894cf9b9cad
edwinwalela <edwinwalela@gmail.com>
2023-01-13 16:33:58 +0300

    add dynamic page header

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -1,9 +1,27 @@
+import { useLocation } from 'react-router-dom';
+
 const Header = () => {
+	const location = useLocation().pathname.split('/');
+	let title = '';
+	switch (location[1]) {
+		case 'people':
+			title = 'People Management';
+			break;
+		case 'analysis':
+			title = 'Analysis';
+			break;
+		case 'settings':
+			title = 'Settings';
+			break;
+
+		default:
+			title = 'Not Found';
+			break;
+	}
+
 	return (
 		<div className="md:flow-root shadow-md py-3 px-8">
-			<h1 className="md:float-left font-medium text-lg text-center md:text-left">
-				People Management
-			</h1>
+			<h1 className="md:float-left font-medium text-lg text-center md:text-left">{title}</h1>
 			<div className="md:float-right md:flex items-center hidden md:block">
 				<img src="https://picsum.photos/200" className="w-8 h-8 rounded-full mr-4" />
 				<div className="">
--- src/pages/EditUser/index.tsx
+++ src/pages/EditUser/index.tsx
@@ -5,8 +5,8 @@ const EditUserPage = () => {
 
 	return (
 		<div className="bg-gray-200 py-8 ">
-			<div className="flex flex-col items-center  px-6 py-8 mx-auto md:h-screen lg:py-0">
-				<div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+			<div className="flex flex-col items-center px-6 mx-auto lg:py-0">
+				<div className="w-full bg-white rounded-lg  shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 						<form className="space-y-4 md:space-y-6" action="#">
 							<div>
@@ -64,10 +64,10 @@ const EditUserPage = () => {
 								>
 									Bio
 								</label>
-								<input
-									type="email"
+								<textarea
 									name="email"
 									id="email"
+									rows={4}
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									placeholder="name@company.com"
 									required={true}
