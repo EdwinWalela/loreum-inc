02459210b617e042d2b30d6470c9a1c1e4b1c32c
edwinwalela <edwinwalela@gmail.com>
2023-01-13 13:23:24 +0300

    reduce table header font

--- src/pages/Landing/tableHeader.tsx
+++ src/pages/Landing/tableHeader.tsx
@@ -1,10 +1,10 @@
 const TableHeader = () => {
 	return (
 		<tr className="bg-gray-300">
-			<th className="w-1/4 px-6 py-2">FIRST NAME</th>
-			<th className="w-1/4 px-6 py-2">LAST NAME</th>
-			<th className="w-1/4 px-6 py-2">OCCUPATION</th>
-			<th className="w-1/4 px-6 py-2">ACTION</th>
+			<th className="w-1/4 px-6 py-2 text-xs">FIRST NAME</th>
+			<th className="w-1/4 px-6 py-2 text-xs">LAST NAME</th>
+			<th className="w-1/4 px-6 py-2 text-xs">OCCUPATION</th>
+			<th className="w-1/4 px-6 py-2 text-xs">ACTION</th>
 		</tr>
 	);
 };
