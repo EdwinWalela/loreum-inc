10ee2016dc54d641e5e6fb36b17b4ba6f4c2d8bd
edwinwalela <edwinwalela@gmail.com>
2023-01-14 11:11:58 +0300

    create user api service

---
+++ src/api/users/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { User } from '../../common/types';
+const BASE_URL = import.meta.env.API_BASE_URL;
+const API_KEY = import.meta.env.API_KEY;
+
+export const getAllUsers = async () => {
+	let users: User[] = [];
+	try {
+		const response = await axios.get(BASE_URL, { headers: { 'x-apikey': API_KEY } });
+		for (const data of response.data) {
+			let user: User = {
+				id: data._id,
+				name: data.name,
+				email: data.email,
+				occupation: data.occupation,
+				bio: data.bio,
+			};
+			users.push(user);
+		}
+	} catch (error: any) {
+		throw new Error(`Failed to fetch users: ${error.message}`);
+	}
+};
